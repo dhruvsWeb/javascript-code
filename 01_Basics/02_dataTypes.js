989c97ea05cc389da42d24552d2e33096bbeaca6
dhruv saini <161109468+dhruvsWeb@users.noreply.github.com>
2024-07-07 10:51:02 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict";// treat all code as new version 
+
+// alert(3+3) we are using nodejs, not browser
+console.log(3+3) //code readbility should be high
+console.log("Hitesh")
+//number => 2 to the power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+// undefined => 
+//symbol 
+//object
+console.log(typeof "hitesh");
+console.log(typeof null);
