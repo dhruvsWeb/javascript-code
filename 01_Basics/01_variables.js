5aaa119b3925feff8e4ec4e64d20bc777aab54ef
dhruv saini <161109468+dhruvsWeb@users.noreply.github.com>
2024-07-07 10:22:17 +0000

    variables code

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const AccountId= 4123
+let AccountEmail= "dhruv@google.com"
+var AccountPassword ="321321"
+AccountCity="chitrakoot"
+
+AccountEmail="dhruv@google.com"
+AccountPassword="212121"
+AccountCity="Bengluru"
+
+console.log(AccountId);
+console.table([AccountId,AccountEmail,AccountCity,AccountPassword])
