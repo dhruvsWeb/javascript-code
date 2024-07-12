75ddff49c9e4197292bbc431e3f90941d226ee5c
dhruv saini <161109468+dhruvsWeb@users.noreply.github.com>
2024-07-12 10:54:42 +0000

    comparisons types

---
+++ 01_Basics/04_Comparisons.js
@@ -0,0 +1,13 @@
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0); //false
+console.log(null==0);// false
+console.log(null>=0); //true
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+//=== strict check 
+console.log("2"===2);
