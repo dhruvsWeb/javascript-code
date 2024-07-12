a3b9b623e987c7bd37c23b9bc833a42492ace742
dhruv saini <161109468+dhruvsWeb@users.noreply.github.com>
2024-07-12 11:33:54 +0000

    Revise data types

---
+++ 01_Basics/datatypesSummary.js
@@ -0,0 +1,32 @@
+// primitive data type 
+// 7 types : string , Number , Boolean,null ,Undefined, symbol,BigInt
+
+// Refrence (Non Primitive )
+//Array , objects , functions 
+
+const score=100
+const scoreValue =100.3
+
+const isLoggedin=false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId);
+
+//const BigNumber =340494802302312n
+//console.log(BigNumber);
+
+const heros=["Shaktimaan","Naagraj","Doga"];
+let Myobj={
+    name:"vaibhav",
+    age: 22,
+}
+
+const MyFunction =function(){
+    console.log("Hello world");
+}
+
+console.log(typeof BigNumber);
+
